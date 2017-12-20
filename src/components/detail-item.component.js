7af793534b068f55b9f47dc9efa1db5a16524815
rvdmolen <ricvdmolen@gmail.com>
2017-12-20 23:22:36 +0100

    changes

--- src/components/detail-item.component.js
+++ src/components/detail-item.component.js
@@ -2,8 +2,7 @@
 
 class DetailItemComponent extends Polymer.Element {
 
-    tick = () => {
-    }
+
 
     static get is() {
         return 'detail-item';
@@ -26,38 +25,49 @@ class DetailItemComponent extends Polymer.Element {
 
     constructor() {
         super();
+        this.DetailItemComponent();
+    }
 
+    DetailItemComponent() {
+        // this.tick = () => {
+        // }
+
+        this.ready = () => {
+            super.ready();
+            // this._onMessage = this._onMessage.bind(this); // to preserve this context
+            this.$.getData.addEventListener('on-message', e => {this._onMessage(e)});
+            // debugger
+            this.$.getData.openConnection();
+            // this.$.websocket.open();
+        };
+
+        this._addItem = (e) => {
+            // this.push('items', e.detail.message);
+            this.items = [...this.items, e.detail.message];
+        };
+
+        this._onMessage = (data) => {
+            console.log(data.detail);
+            console.log(this.$);
+            // this.push('items', data.detail);
+            this.items = [...this.items, data.detail];
+
+            // this.dispatchEvent(new CustomEvent('ing-add-item', {
+            //     detail: {message: data.data},
+            //     bubbles: true,
+            //     composed: true
+            // }));
+        };
     }
 
 
-    ready() {
-        super.ready();
-        // this._onMessage = this._onMessage.bind(this); // to preserve this context
-        this.$.getData.addEventListener('on-message', e => {this._onMessage(e)});
-        // debugger
-        this.$.getData.openConnection();
-        // this.$.websocket.open();
-    }
-
-    _addItem(e) {
-        // this.push('items', e.detail.message);
-        this.items = [...this.items, e.detail.message];
-    }
 
 
 
-    _onMessage = (data) => {
-        console.log(data.detail);
-        console.log(this.$);
-       // this.push('items', data.detail);
-        this.items = [...this.items, data.detail];
 
-        // this.dispatchEvent(new CustomEvent('ing-add-item', {
-        //     detail: {message: data.data},
-        //     bubbles: true,
-        //     composed: true
-        // }));
-    }
+
+
+
 }
 
 window.customElements.define(DetailItemComponent.is, DetailItemComponent);
